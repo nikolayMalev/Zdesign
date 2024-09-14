ff15941da80c1b3d3c12438f3d75561008035b5a
nikolayMalev <nikimalev98@gmail.com>
2024-09-14 18:48:23 +0300

    Add splidejs

--- wp-content/themes/zdesign/header.php
+++ wp-content/themes/zdesign/header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href='<?php echo get_stylesheet_directory_uri();?>/dist/css/styles.min.css'/>
+    <link href='<?php echo get_stylesheet_directory_uri();?>/resources/js/main.js'/>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- wp-content/themes/zdesign/templates/home.php
+++ wp-content/themes/zdesign/templates/home.php
@@ -18,4 +18,28 @@ Template Name: Homepage
     </div><!-- /.shell-->
 </section><!-- /.hero -->
 
+<section class="section-image-content">
+    <div class="section__content">
+        <h2>Вашият Дом. Вашият Стил</h2>
+
+        <ul>
+            <li>Ще Ви спестим време</li>
+
+            <li>Ще Ви изготвим проект</li>
+
+            <li>Ще го реализираме за Вас</li>
+        </ul>
+
+        <div class="section__actions">
+            <h4>Какво чакате?</h4>
+
+            <a href="" class="btn">Заяви Проект</a>
+        </div><!-- /.section__actions -->
+    </div><!-- /.section__content -->
+
+    <div class="section__image">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/image-content.png" alt="" class="">
+    </div><!-- /.section__image -->
+</section><!-- /.section-image-content -->
+
 <?php get_footer(); ?>
